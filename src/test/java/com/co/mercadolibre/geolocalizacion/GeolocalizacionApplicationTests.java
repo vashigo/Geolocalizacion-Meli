c24d3a1373a067de709d9fb438ac6127b27670f5
Andres David Vasquez <andresdavid.vasquez@gmail.com>
2024-08-27 00:04:06 -0500

    Configuracion inicial y proyecto base

---
+++ src/test/java/com/co/mercadolibre/geolocalizacion/GeolocalizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.mercadolibre.geolocalizacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeolocalizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
